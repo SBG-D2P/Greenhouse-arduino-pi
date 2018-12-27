55eb771c862fb528f75ce40990c7d130ae4c901c
SBG-D2P <43755713+SBG-D2P@users.noreply.github.com>
2018-12-27 11:41:22 +0100

    Update Demux first layer.ino

--- Arduino/Demux first layer.ino
+++ Arduino/Demux first layer.ino
@@ -22,10 +22,9 @@ int ON = HIGH;
 //----------------------------------------------------------------------------------------------------
 
 //--------------------------------------PINS-SETUP-RELAY-PAIRS----------------------------------------
-int First = A0; // Pin A0 as pin for plugged detectors
 int Second = A1; // sensor pin for incoming data
 
-int Layer1 = 0;  //value used to see if sensors are plugged
+int Layer2 = 0;//value used to see if sensors are plugged and to gather data
 
 int S0 = 10;   //output pin for demux control of the probe (probe selector)
 int S1 = 11;
@@ -33,13 +32,14 @@ int S2 = 12;
 int S3 = 13;
 int E = 9;
 
-int SS0 = 8;   //output pin for demux control of the moiture detection (remote demux on the probe)
-int SS1 = 7;
-int SS2 = 6;
-int SS3 = 5;
+int SS0 = 5;   //output pin for demux control of the moiture detection (remote demux on the probe)
+int SS1 = 6;
+int SS2 = 7;
+int SS3 = 8;
 int E2 = 4;
 
 int TwoDDataArray[16][16];//array to store data from all measured sensors
+//uint16_t TwoDDataArray[16];
 
 //----------------------------------------------------------------------------------------------------
 
@@ -68,37 +68,47 @@ int DEMUX[16][4] = { // change order to match physical 1,2,3...
 void Plugged()
 {
   au16data[4] = 0;//storing which probes are plugged in in a "binary" fashion 1 == only first; 9 == first and third (0000000000000101) I think negative is for the last one
-  for (int i = 0; i < 16; ++i) {// loops through probes
 
+    digitalWrite(E2, HIGH);
+    digitalWrite(SS0, DEMUX[0][0]);
+    digitalWrite(SS1, DEMUX[0][1]);
+    digitalWrite(SS2, DEMUX[0][2]);
+    digitalWrite(SS3, DEMUX[0][3]);
+    digitalWrite(E2, LOW);
+  
+  for (int i = 0; i < 16; ++i) {// loops through probes
+    
+    digitalWrite(E, HIGH);
     digitalWrite(S0, DEMUX[i][0]);
     digitalWrite(S1, DEMUX[i][1]);
     digitalWrite(S2, DEMUX[i][2]);
     digitalWrite(S3, DEMUX[i][3]);
     digitalWrite(E, LOW);
     delay(10);
-    Layer1 = analogRead(First); // stores temperary if there was current flowing
-    //Serial.println(Layer1);
+    
     delay(5);
 
-    if (Layer1 > 300) { // adds probe to binary list if there was current flowing
-      au16data[4] = au16data[4] + pow(2, i);// adds probe to number in binary form
+    if (Layer2 > 350) { // adds probe to binary list if there was current flowing
+      //au16data[4] = Layer1;
+      au16data[4] = au16data[4] + round(pow(2, i));// adds probe to number in binary form needs rounding otherwise returns too small value from floating point
     }
     digitalWrite(E, HIGH);
     delay (5);
   }
-
+ digitalWrite(E2, HIGH);
+ au16data[1] = 0;
 }
 //------------------------------------------------------------------------------------------------------
 
 //-------------------------------------------Measuring--------------------------------------------------
-void Measuring(uint16_t& x) {
-
+void Measuring(uint16_t& p) {
+  int x = p;
   digitalWrite(E, HIGH);        //Selecting desired probe out of 16
   digitalWrite(S0, DEMUX[x][0]);
   digitalWrite(S1, DEMUX[x][1]);
   digitalWrite(S2, DEMUX[x][2]);
   digitalWrite(S3, DEMUX[x][3]);
-  digitalWrite(E, LOW);
+  //digitalWrite(E, LOW);
 
   for (int k = 0; k < 16; ++k) {    //getting measurment on all 16 levels of the probe
 
@@ -107,22 +117,29 @@ void Measuring(uint16_t& x) {
     digitalWrite(SS1, DEMUX[k][1]);
     digitalWrite(SS2, DEMUX[k][2]);
     digitalWrite(SS3, DEMUX[k][3]);
+    digitalWrite(E, LOW);
     digitalWrite(E2, LOW);
 
     delay(10);  //delay to let pads electrify properly
-    TwoDDataArray[x][k] = analogRead(Second); //storing data in main array
-    //Serial.println(Layer1);
+    //TwoDDataArray[x][k] = analogRead(Second); //storing data in main array
+    int Layer2 = analogRead(Second);
+    TwoDDataArray[x][k] = Layer2; //analogRead(Second); //storing data in main array
+    //slave.poll( au16data, 16 ); //put 40 miliseconds delay in the pythonscript
+  
     delay(5);
   }
+   au16data[1] = 0;
+   digitalWrite(E, HIGH);
+   digitalWrite(E2, HIGH);
 }
 //-----------------------------------------------------------------------------------------------------
 
 //---------------------------------------------Data-Transfert------------------------------------------
-void DataTransfert(uint16_t& y, uint16_t& z) {
-  for (int i = 0; i < 16; i++) { //loops through all values of a given probe
-    z = TwoDDataArray[y][i];
-    slave.poll( au16data, 16 ); //put 40 miliseconds delay in the pythonscript
-  }
+void DataTransfert(uint16_t& x, uint16_t& z, uint16_t& y) {  
+  //for (uint16_t i = 0; i < 16; i++) { //loops through all values of a given probe
+    //z = TwoDDataArray[x][i];
+    z = TwoDDataArray[x][y];
+  //}
   au16data[1] = 0;
 }
 //-----------------------------------------------------------------------------------------------------
@@ -135,7 +152,6 @@ void setup()
   pinMode(S2, OUTPUT);
   pinMode(S3, OUTPUT);
   pinMode(E, OUTPUT);
-  pinMode(First, INPUT);
 
   pinMode(SS0, OUTPUT);
   pinMode(SS1, OUTPUT);
@@ -144,7 +160,12 @@ void setup()
   pinMode(E2, OUTPUT);
   pinMode(Second, INPUT);
 
+  digitalWrite(E, HIGH);
+  digitalWrite(E2, HIGH);
+
   slave.begin( 19200 ); // baud-rate at 19200
+
+  Plugged();//check which probes are plugged when device is started or reseted --  must be last line of setup
 }
 //-----------------------------------------------------------------------------------------------------
 
@@ -170,7 +191,7 @@ void loop()
   }
 
   else if (au16data[1] == 3) {
-    DataTransfert(au16data[2], au16data[3]);//(probe being red, temporary shared data for probe)
+    DataTransfert(au16data[2], au16data[3], au16data[7]);//(Probe being red, Value(level) being red, Indicates which level to read)
   }
 }
 //=====================================================================================================
