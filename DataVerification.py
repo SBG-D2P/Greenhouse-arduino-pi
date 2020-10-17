e879758fc12cc41668f654bbc8d20ea7d375974c
SBG-D2P <43755713+SBG-D2P@users.noreply.github.com>
2020-10-17 19:21:23 +0200

    Create DataVerification.py

---
+++ DataVerification.py
@@ -0,0 +1,12 @@
+import subprocess
+import time
+import datetime
+import serial
+import pandas as pd
+import numpy as np
+
+ df3 = pd.read_csv('Data.csv',names=columns, header=None)
+df4[df.probeID = 'ProbeID'] #probeID is temporary and should be replaced with the actuall column's name and variable
+df5 = df4.tail(2)
+
+ 
