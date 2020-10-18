2fc0a0537d7032556ba6f556e3aa3dbb8f8c17d3
SBG-D2P <43755713+SBG-D2P@users.noreply.github.com>
2020-10-18 18:15:04 +0200

    Update DataVerification.py

--- DataVerification.py
+++ DataVerification.py
@@ -9,4 +9,15 @@ import numpy as np
 df4[df.probeID = 'ProbeID'] #probeID is temporary and should be replaced with the actuall column's name and variable
 df5 = df4.tail(2)
 df6 = df4[1] - df4[2]
-df6.drop(columns=18)
+df6.drop(columns=18)#columns='R' maybe?
+
+for i in range(17,18):#loops to check temperature and humidity
+ if df6[i] > 0.1:
+  #email
+ else:return
+
+for j in range(0,16):#loops 
+ if df6[j] > 0.1:
+  #email
+  break #needed to terminated loop and avoid 16 notification for a single failure
+ else:return
