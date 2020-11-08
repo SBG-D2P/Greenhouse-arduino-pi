bfbf5f6e45b913c116ae5e750dced77a8845a1e6
SBG-D2P <43755713+SBG-D2P@users.noreply.github.com>
2020-11-08 16:48:05 +0100

    Create DataVerification.py

--- DataVerification.py
+++ DataVerification.py
@@ -5,19 +5,44 @@ import serial
 import pandas as pd
 import numpy as np
 
- df3 = pd.read_csv('Data.csv',names=columns, header=None)
-df4[df.probeID = 'ProbeID'] #probeID is temporary and should be replaced with the actuall column's name and variable
-df5 = df4.tail(2)
-df6 = df4[1] - df4[2]
-df6.drop(columns=18)#columns='R' maybe?
+#Indexes = range(100)
+Columns = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S']#might need to remove extra letters (only ABC)
+CurrentDevices = pd.read_csv('Devices.csv', names = Columns)
+df3 = pd.read_csv('Data.csv',names = Columns, header=None, sep= '\t')
 
-for i in range(17,18):#loops to check temperature and humidity
- if df6[i] > 0.1:
-  #email
- else:return
 
-for j in range(0,16):#loops 
- if df6[j] > 0.1:
-  #email
-  break #needed to terminated loop and avoid 16 notification for a single failure
- else:return
+for i in range (100):#checks each devices if water hub
+    if CurrentDevices.at[i,'A'] == 2:
+        
+        CurrentProbes = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] # array to check which probes are plugged
+        num = int(CurrentDevices.at[i,'C'])
+        for n in range(1,17):#convert number to plugged probes
+            bitpos = n
+            CurrentProbes[n-1] = (num >> (bitpos-1))&1
+        
+        for m in range(1,16):# verify each probes of each devices
+            if CurrentProbes[m] == 1:
+                Tnum = (str(i) + '-' + str(m))
+                #print(df3)
+                df4 = df3[df3['R'] == Tnum]#creates new datafram with only data from single probe
+                #print(df4)
+                df5 = df4.tail(2)
+                df5.reset_index(inplace=True)
+                df5 = df5.drop(columns=['R','S'])#columns='R' maybe?
+                #print(df5)
+                df5 = df5.astype(int)
+                df6 = df5.loc[0] - df5.loc[1]
+                print(df6)
+
+
+                for p in range(17,18):#loops to check temperature and humidity
+                    if df6[p] > 0.1:
+                        print('test')
+                        #email
+
+                for q in range(0,16):#loops 
+                    if df6[q] > 0.1:
+                        print('test2')
+                        #email
+                        break #needed to terminated loop and avoid 16 notification for a single failure
+
